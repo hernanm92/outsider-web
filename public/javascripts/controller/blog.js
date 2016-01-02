3bf97ee21c79223f596aa0d29b048be6a24dbf6b
Hernan Maschwitz <hernanm992@gmail.com>
2016-01-01 22:29:22 -0300

    blog in angular

--- public/javascripts/controller/blog.js
+++ public/javascripts/controller/blog.js
@@ -1,5 +1,19 @@
 app.controller('BlogController',
-    function ($scope, homeFactory, eventService) {
+    function ($scope, homeFactory, eventService, $sce) {
+
+        $scope.posts = [
+            {url: 'img/blog/img1.jpg', label: 'skate1', type: 'image'},
+            {url: 'img/blog/img2.jpg', label: 'longboard2', type: 'image'},
+            {url: '//player.vimeo.com/video/113463529?title=0&byline=0&portrait=0', label: 'surf3', type: 'video'},
+            {url: 'img/blog/img2.jpg', label: 'longboard4', type: 'image'},
+            {url: 'img/blog/img1.jpg', label: 'skate5', type: 'image'}
+        ]
+
+        $scope.getPostUrl = function(post){
+            return $sce.trustAsResourceUrl(post.url);
+        }
+
+
 
         $scope.$on('$viewContentLoaded', function(){
 
