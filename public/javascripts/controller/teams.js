add34500c7ff12a01ba4de8d021d7eb0b6a07ec4
Hernan Maschwitz <hernanm992@gmail.com>
2016-01-02 02:05:11 -0300

    angular team view

--- public/javascripts/controller/teams.js
+++ public/javascripts/controller/teams.js
@@ -1,6 +1,13 @@
 app.controller('TeamsController',
     function ($scope, homeFactory, eventService) {
 
+        $scope.riders = [
+            {photo_url: 'img/teams/img1.png', sport: 'longboard', name: 'Damián Nehemias'},
+            {photo_url: 'img/teams/img2.png', sport: 'longboard', name: 'Sebastián Muñoz'},
+            {photo_url: 'img/teams/img3.png', sport: 'longboard', name: 'Thomas Duarte'},
+            {photo_url: 'img/teams/img4.png', sport: 'longboard', name: 'Valentín Travis Spalla'}
+        ]
+
         $scope.$on('$viewContentLoaded', function(){
             App.init();
             //App.initParallaxBg();
