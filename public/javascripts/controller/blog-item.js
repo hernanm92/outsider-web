ae80820f3dd7e793ec56e5b41a4675caf1715f84
Hernan Maschwitz <hernanm992@gmail.com>
2016-01-02 01:30:41 -0300

    blog item view

--- public/javascripts/controller/blog-item.js
+++ public/javascripts/controller/blog-item.js
@@ -2,7 +2,13 @@ app.controller('BlogItemController',
     function ($scope, homeFactory, eventService, $sce) {
 
         $scope.$on('$viewContentLoaded', function(){
-
+            App.init();
+            LoginForm.initLoginForm();
+            ContactForm.initContactForm();
         });
+
+        $scope.comment = function() {
+            console.log("plaaaa")
+        }
     }
 );
