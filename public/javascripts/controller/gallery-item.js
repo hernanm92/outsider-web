010cb724f63238574a0a4608e8073d027968530c
Hernan Maschwitz <hernanm992@gmail.com>
2016-01-05 21:09:22 -0300

    dynamic gallery

--- public/javascripts/controller/gallery-item.js
+++ public/javascripts/controller/gallery-item.js
@@ -1,9 +1,17 @@
 app.controller('GalleryItemController',
-    function ($scope, homeFactory, eventService) {
+    function ($scope, galleryFactory, eventService, $routeParams) {
 
         $scope.$on('$viewContentLoaded', function(){
             App.init();
             OwlRecentWorks.initOwlRecentWorksV1();
         });
+
+        $scope.getPhoto = function(){
+            galleryFactory.get({"id": $routeParams.photo}, function(photo){
+                $scope.photo = photo;
+            });
+        }
+
+        $scope.getPhoto();
     }
 );
