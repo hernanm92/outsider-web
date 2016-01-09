a462212fa3486121cdd72e614df4006041b563fc
Hernan Maschwitz <hernanm992@gmail.com>
2016-01-09 00:42:44 -0300

    change gallery analytics

--- public/javascripts/controller/gallery.js
+++ public/javascripts/controller/gallery.js
@@ -1,5 +1,5 @@
 app.controller('GalleryController',
-    function ($scope, galleryFactory, eventService) {
+    function ($scope, galleryFactory, eventService, $location, $window) {
 
         $scope.$on('$viewContentLoaded', function(){ //Template javascript (JQuery must be in angular controllers)
             App.init();
@@ -12,6 +12,19 @@ app.controller('GalleryController',
         }
 
         $scope.getPhotos();
+
+        //Load Google Analytics
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-72174864-1', 'auto');
+        //ga('send', 'pageview');
+
+        $window.ga('send', 'pageview', {
+            page: $location.path()
+        });
 
     }
 );
