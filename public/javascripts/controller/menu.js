65b1e4f282bb5698ca9fb7e281cb2d8161cc8b28
Hernan Maschwitz <hernanm992@gmail.com>
2016-01-09 02:19:06 -0300

    selected menu

--- public/javascripts/controller/menu.js
+++ public/javascripts/controller/menu.js
@@ -1,3 +1,6 @@
 app.controller('MenuController', function ($scope, homeService, $controller, $location,  $timeout, $rootScope, eventService, config) {
+    $scope.isNavActive = function(id) {
+        return $location.path() == id
+    };
 
 });
