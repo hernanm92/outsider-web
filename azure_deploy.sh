d7fa128ae165f8fa3c79384f34e982edfd94de3f
Hernan Maschwitz <hernanm992@gmail.com>
2016-03-13 19:54:55 -0300

    validating os

---
+++ azure_deploy.sh
@@ -0,0 +1,9 @@
+echo "starting deploy script"
+uname
+#@echo off
+#echo Deploying files...
+#xcopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET% /Y
+
+#npm install
+#node -e "require('grunt').tasks(['production']);"
+#node app.js
